06b4a82eeab2f8ebdedca689380b304b4154d3f7
dxia2 <90279328+dxia2@users.noreply.github.com>
2021-09-20 13:07:38 -0600

    Delete WebsiteProject directory

--- WebsiteProject/WebsiteProject/quizScript.js
+++
@@ -1,8 +0,0 @@
-// Chairs Quiz
-let checkAnswersButton = document.getElementById("checkAnswersButton");
-
-
-setInterval(randomColorButton, 3000);
-function randomColorButton(){
-    checkAnswersButton.style.backgroundColor = rgba(255 * Math.random(), 255 * Math.random(), 255 * Math.random(), 1);
-}
